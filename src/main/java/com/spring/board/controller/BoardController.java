bf06b79dd5bbb6372642b0c08244beb878ce0744
tychejin1218 <tychejin@naver.com>
2021-06-02 12:53:56 +0900

    [Spring] [Spring] 게시판 만들기(14) - 게시글 조회(SELECT)  - 모델2 방식 MVC 패턴 파일 추가

---
+++ src/main/java/com/spring/board/controller/BoardController.java
@@ -0,0 +1,38 @@
+package com.spring.board.controller;
+ 
+import java.util.List;
+ 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+ 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+ 
+import com.spring.board.dto.BoardDto;
+import com.spring.board.form.BoardForm;
+import com.spring.board.service.BoardService;
+ 
+@Controller
+@RequestMapping(value = "/board")
+public class BoardController {
+ 
+    @Autowired
+    private BoardService boardService;
+ 
+    @RequestMapping( value = "/boardList")
+    public String getBoardList(HttpServletRequest request, HttpServletResponse response) throws Exception{
+        
+        return "board/boardList";
+    }
+ 
+    @RequestMapping(value = "/getBoardList")
+    @ResponseBody
+    public List<BoardDto> getBoardList(HttpServletRequest request, HttpServletResponse response, BoardForm boardForm) throws Exception {
+ 
+        List<BoardDto> boardList = boardService.getBoardList(boardForm);
+ 
+        return boardList;
+    }
+}
