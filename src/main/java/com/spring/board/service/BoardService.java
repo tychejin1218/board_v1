e77e31e772dd582c7df5fe74616e1e0df6a46d41
tychejin1218 <tychejin@naver.com>
2021-07-04 15:12:56 +0900

    [Spring] 게시판 만들기(22) - MDC(Mapped Diagnostic Context) 설정  - logback.xml 수정  - BoardController.java 수정  - BoardService.java 수정

--- src/main/java/com/spring/board/service/BoardService.java
+++ src/main/java/com/spring/board/service/BoardService.java
@@ -5,7 +5,9 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.UUID;
- 
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -22,6 +24,8 @@ import com.spring.board.form.CommonForm;
 @Service
 public class BoardService {
  
+	protected final Logger logger = LoggerFactory.getLogger(BoardService.class);
+	
     @Autowired
     private BoardDao boardDao;
  
@@ -69,6 +73,8 @@ public class BoardService {
     /** 게시판 - 상세 조회 */
     public BoardDto getBoardDetail(BoardForm boardForm) throws Exception {
  
+    	logger.debug("==================== getBoardDetail START ====================");
+    	
         BoardDto boardDto = new BoardDto();
  
         String searchType = boardForm.getSearch_type();
@@ -91,6 +97,8 @@ public class BoardService {
  
         boardDto.setFiles(boardDao.getBoardFileList(boardFileForm));
  
+        logger.debug("==================== getBoardDetail END ====================");
+        
         return boardDto;
     }
  
